4439349cc1c897374c93c3c23bd75243b0ba6398
Raju-Talukder <rajutalukder70@gmail.com>
2020-08-27 20:25:00 +0600

    Problem 1002

---
+++ src/com/solve/Problem1002.java
@@ -0,0 +1,11 @@
+package com.solve;
+
+import java.util.Scanner;
+
+public class Problem1003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        System.out.printf("A=%.4f\n",(3.14159*a*a));
+    }
+}
